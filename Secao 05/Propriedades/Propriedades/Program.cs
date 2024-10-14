b721d872cede2aac08b72c3a49dbba0430a31f66
Hugo <hugosantosadm125@gmail.com>
2024-10-14 17:00:46 -0300

    Adição de conteúdos de Encapsulamento, Propriedades, AutoProperties

---
+++ Secao 05/Propriedades/Propriedades/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Propriedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500, 10);
+
+            p.Nome = "TDDDDDS";
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+            Console.WriteLine(p.Quantidade);
+        }
+    }
+}
