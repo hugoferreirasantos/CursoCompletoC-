e4116687adec19437f77712b85b6ec87784743a1
Hugo <hugosantosadm125@gmail.com>
2024-10-20 19:50:01 -0300

    Exemplo de Struct no C#

---
+++ Secao 06/ExemploStruct/ExemploStruct/Point.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ExemploStruct
+{
+    struct Point
+    {
+        public double X, Y;
+
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+    }
+}
---
+++ Secao 06/ExemploStruct/ExemploStruct/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExemploStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p = new Point(); //Pode só declarar a variável do tipo: Point sem o New
+            p.X = 10;
+            p.Y = 20;
+            Console.WriteLine(p);
+
+
+        }
+    }
+}
