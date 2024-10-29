b26160419969f78b60763b6aa7606a4f92f000e9
Hugo <hugosantosadm125@gmail.com>
2024-10-28 21:04:43 -0300

    Exemplos de Nullable e Coalecencia nula

---
+++ Secao 06/CoalescenciaNula/CoalescenciaNula/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace CoalescenciaNula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double? x = null;
+            double? z = 50.0;
+            double? y = x ?? 5.0;
+            double? a = z ?? 0.0;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ Secao 06/Nullable/Nullable/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nullable<double> b = null; pode usar desse jeito
+
+            double? x = null;
+            double? y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            if (x.HasValue)
+            {
+                Console.WriteLine(x.Value);
+            }
+            else
+            {
+                Console.WriteLine("X is null");
+            }
+
+            if (y.HasValue)
+            {
+                Console.WriteLine(y.Value);
+            }
+            else
+            {
+                Console.WriteLine("Y is null");
+            }
+        }
+    }
+}
