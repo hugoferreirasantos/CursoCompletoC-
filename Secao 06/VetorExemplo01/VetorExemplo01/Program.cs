e4f29066ada0e9dde5797cb693183278dfe77ac9
Hugo <hugosantosadm125@gmail.com>
2024-11-04 21:19:54 -0300

    Exemplo de Vetor 01

---
+++ Secao 06/VetorExemplo01/VetorExemplo01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace VetorExemplo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tamanho = int.Parse(Console.ReadLine()); //Pega o tamanho do vetor
+
+            double[] vetor = new double[tamanho]; //Instancia o vetor
+
+            for ( int i = 0; i < tamanho; i++ )
+            {
+                vetor[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            } //Inseri os valores nas posições do vetor 
+
+            double soma = 0.0;
+
+            for ( int i = 0; i < tamanho; i++)
+            {
+                soma += vetor[i];
+            }//Soma os valores das alturas que estão no vetor
+
+            double media = soma / tamanho;//Monta a media
+
+            Console.WriteLine($"AVERAGE HEIGHT {media.ToString("F2", CultureInfo.InvariantCulture)}"); //Imprime a media
+
+        }
+    }
+}
