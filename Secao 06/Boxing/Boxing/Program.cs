1795534cfba5f0d48d269ec63fac99f4c127a7f9
Hugo <hugosantosadm125@gmail.com>
2024-11-14 18:46:42 -0300

    Entendendo Boxing e Unboxing

---
+++ Secao 06/Boxing/Boxing/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20;
+            Object obj = x;
+
+            Console.WriteLine(obj);
+            Console.WriteLine(obj.GetType());
+        }
+    }
+}
---
+++ Secao 06/Unboxing/Unboxing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20;
+            Object obj = x;
+            int y = (int) obj;
+
+            Console.WriteLine(y);
+            Console.WriteLine(y.GetTypeCode());
+        }
+    }
+}
