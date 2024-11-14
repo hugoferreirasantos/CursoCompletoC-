d5b824edd48f5c4f5bb7e7540eb8454a7d393e56
Hugo <hugosantosadm125@gmail.com>
2024-11-14 18:59:20 -0300

    Sintaxe do Foreach

---
+++ Secao 06/LacoForeach/LacoForeach/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LacoForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vetor = new string[] { "Maria", "Bob", "Alex" };
+
+            foreach (string obj in vetor)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
