6576499eab4babf883b7f3e4e9ae193b6d57c3a8
Hugo <hugosantosadm125@gmail.com>
2024-11-25 21:02:03 -0300

    Exemplo de Matriz

---
+++ Secao 06/Matrizes/Matrizes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] mat = new double[2, 3];
+
+            Console.WriteLine(mat.Length); // retorna o tamanho da matriz: linhas x colunas:
+            Console.WriteLine(mat.Rank); // retorna o número de dimenções de array:
+            Console.WriteLine(mat.GetLength(0)); // retorna o número de linhas
+            Console.WriteLine(mat.GetLength(1)); // retorna o número de colunas
+        }
+    }
+}
