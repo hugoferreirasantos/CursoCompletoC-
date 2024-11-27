03d60d8d5097e0a3fa7329ccc34699d6a58fbbde
Hugo <hugosantosadm125@gmail.com>
2024-11-27 20:43:11 -0300

    Exercicio Resolvido de Matriz

---
+++ Secao 06/ExercicioResovidoMatriz/ExercicioResovidoMatriz/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ExercicioResolvidoMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n]; //Instanciando a matriz
+
+            //Inserindo valores dentro da matriz:
+            for ( int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                for ( int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            //Mostrando os valores da diagonal principal:
+            Console.WriteLine("Main diagonal: ");
+            for ( int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i,i] + " ");
+            }
+
+            Console.WriteLine();
+
+            //Contabilizando os valores negativo:
+            int count = 0;
+            for ( int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Negative numbers: {count}");
+
+        }
+    }
+}
