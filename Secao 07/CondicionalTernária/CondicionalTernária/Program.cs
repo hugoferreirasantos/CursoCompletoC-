1b430758e646e11954009b4a9117f2dc470eb729
Hugo <hugosantosadm125@gmail.com>
2024-12-09 18:52:37 -0300

    Exemplos de Condição ternária, Palavra Var, SwitchCase

---
+++ Secao 07/CondicionalTernária/CondicionalTernária/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CondicionalTernaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe um preço: ");
+            double preco = double.Parse(Console.ReadLine());
+
+            double desconto = (preco < 20.0) ? preco * 0.01 : preco * 0.05;
+            Console.WriteLine($"Desconto: {desconto}");
+        }
+    }
+}
