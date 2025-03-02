37b338a9eb5b91edd4e9533a252e15f2c1b0da73
Hugo <hugosantosadm125@gmail.com>
2025-03-02 19:45:19 -0300

    Totalizador de horas

---
+++ Secao 07/TotalizadorDeHoras/TotalizadorDeHoras/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Xml;
+
+namespace TotalizadorDeHoras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalHoras = 0;
+            int totalMinutos = 0;
+
+            Console.Write("Quantidade vezez deseja somar ? : ");
+            int qtdHoras = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for (int i = 0; i < qtdHoras; i++)
+            {
+                Console.Write("Informe a hora: ");
+                int hora = int.Parse(Console.ReadLine());
+                Console.Write("Informe o minuto: ");
+                int minuto = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+
+                // Adiciona as horas informadas
+                totalHoras += hora;
+                totalMinutos += minuto;
+
+                // Converte os minutos para horas (se 60 minutos = 1 hora) e adiciona ao total
+                totalHoras += totalMinutos / 60;
+                totalMinutos = totalMinutos % 60;
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine($"{totalHoras} horas e {totalMinutos} minutos");
+
+
+        }
+    }
+}
