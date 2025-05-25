857583677f97a146ffd7c83d00c4d80eb12832f0
Hugo Ferreira <hugosantosadm125@gmail.com>
2025-05-25 15:58:01 -0300

    exercicios de Upcasting e Downcasting

---
+++ Secao 09/Heranca/Heranca/Program.cs
@@ -0,0 +1,18 @@
+﻿using Heranca.Entities;
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount acount = new BusinessAccount(8025,"Hugo", 100.0,500.0);
+
+            //acount.Balance = 200.0; não vai funcionar porque o atributo Balance é PROTECTED:
+
+            Console.WriteLine(acount.Balance);
+
+
+        }
+    }
+}
