47329770e58670c8f9bb32ca2becb7ae8d93c85c
Hugo Ferreira <hugosantosadm125@gmail.com>
2025-07-03 21:25:04 -0300

    Exercicio de Polimorfismo

---
+++ Secao 09/Polimorfismo/ExerciciosResolvidoPolimorfismo/ExerciciosResolvidoPolimorfismo/Program.cs
@@ -0,0 +1,54 @@
+﻿using ExerciciosResolvidoPolimorfismo.Entities;
+using System.Globalization;
+
+namespace ExercicioResolvidoPolimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) 
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Employee #{i} data: ");
+                Console.Write("Outsourced (y/n)? : ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                if( ch == 'y')
+                {
+                    //Adicionando itens na lista:
+                    Console.Write("Additional charge: ");
+                    double addicionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, addicionalCharge));
+                }
+                else
+                {
+                    //Adicionando itens na lista:
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS:");
+
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine($"{emp.Name} - $ {emp.Payment().ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+
+        }
+    }
+}
+
+
